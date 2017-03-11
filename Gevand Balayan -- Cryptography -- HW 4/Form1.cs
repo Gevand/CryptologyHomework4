4725d149587b7ca19d490b3e209730232bef5dc5
Gevand <gevand@outlook.com>
2017-03-11 09:22:29 -0800

    First Commit

---
+++ Gevand Balayan -- Cryptography -- HW 4/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gevand_Balayan____Cryptography____HW_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
